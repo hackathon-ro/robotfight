61efaf8f1b5371d2bb83733b95b012e19cc0fbc9
George Jingoiu <george161089@yahoo.com>
2012-10-28 06:22:38 +0200

    Merged work. Added robo hash , compass

---
+++ client/RobotFight/RobotFight/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  RobotFight
+//
+//  Created by George Jingoiu on 10/28/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@end
---
+++ client/RobotFight/RobotFight/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  RobotFight
+//
+//  Created by George Jingoiu on 10/28/12.
+//
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
